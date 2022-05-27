e6bfce106857708d899ed0d279f911fdfce05ff4
om02op <83261832+om02op@users.noreply.github.com>
2022-05-27 15:23:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ dis_width = 800
 dis_height = 600
  
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game by Edureka')
+pygame.display.set_caption('Snake game by OmR#2222')
  
 clock = pygame.time.Clock()
  
